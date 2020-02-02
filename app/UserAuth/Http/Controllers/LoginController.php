6328e6af84c6c6cd5f82fc8c899d45451b8d3e22
lsurma <kontakt@lsurma.pl>
2020-02-02 20:17:19 +0100

    Login will check if given user has verified its email before logging in

--- app/UserAuth/Http/Controllers/LoginController.php
+++ app/UserAuth/Http/Controllers/LoginController.php
@@ -4,7 +4,9 @@ namespace App\UserAuth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        // Basic credentials from form request
+        $credentials = $request->only($this->username(), 'password');
+
+        // Custom credentials requirements via closure
+        $credentials[] = function(Builder $query) {
+            $query->whereNotNull('email_verified_at');
+        };
+
+        return $credentials;
     }
 }
